f95804a35a337e2a85c5b74afb758b64c133735d
camenduru <54370274+camenduru@users.noreply.github.com>
2023-05-24 07:07:37 +0300

    Python 3.8

--- modules/diffusion/pipelines/tensorrt.py
+++ modules/diffusion/pipelines/tensorrt.py
@@ -216,14 +216,14 @@ class TensorRTStableDiffusionPipeline(DiffusersPipeline):
         unet_in_channels: int,
         image: Optional[torch.Tensor] = None,
         timestep: Optional[torch.Tensor] = None,
-        batch_size: int = 1,
-        height: int = 256,
-        width: int = 256,
+        batch_size: int,
+        height: int,
+        width: int,
         dtype: Optional[torch.dtype] = None,
         generator: Optional[Generator] = None,
         latents: Optional[torch.Tensor] = None,
-    ) -> torch.Tensor:
-        dtype = dtype or torch.float32
+    ):
+        dtype = torch.float32
         if image is not None and self.full_acceleration:
             image = image.to(self.device).to(dtype)
             image = image.repeat(batch_size, 1, 1, 1)
