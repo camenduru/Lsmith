088c40c5ca46260f5fb8ff8fce6af662f9fcdb7e
camenduru <54370274+camenduru@users.noreply.github.com>
2023-05-24 06:09:11 +0300

    Python 3.8

--- modules/diffusion/pipelines/tensorrt.py
+++ modules/diffusion/pipelines/tensorrt.py
@@ -189,8 +189,8 @@ class TensorRTStableDiffusionPipeline(DiffusersPipeline):
         num_images_per_prompt,
         do_classifier_free_guidance,
         negative_prompt=None,
-        prompt_embeds: torch.FloatTensor | None = None,
-        negative_prompt_embeds: torch.FloatTensor | None = None,
+        prompt_embeds: Optional[torch.FloatTensor] = None,
+        negative_prompt_embeds: Optional[torch.FloatTensor] = None,
     ):
         return (
             super()
@@ -214,14 +214,14 @@ class TensorRTStableDiffusionPipeline(DiffusersPipeline):
         self,
         vae_scale_factor: int,
         unet_in_channels: int,
-        image: torch.Tensor | None,
+        image: Optional[torch.Tensor] = None,
         timestep: torch.Tensor,
         batch_size: int,
         height: int,
         width: int,
         dtype: torch.dtype,
         generator: Generator,
-        latents: torch.Tensor = None,
+        latents: Optional[torch.Tensor] = None,
     ):
         dtype = torch.float32
         if image is not None and self.full_acceleration:
